78b0a1ef01628037306d94b66f01a9c98a2c5e91
Waldir <waliuxd@gmail.com>
2026-06-21 03:34:22 -0500

    fix(frontend): use vite-plugin-babel-macros for Lingui JSX macros

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { lingui } from "@lingui/vite-plugin";
-import babel from "@rollup/plugin-babel";
+import macrosPlugin from "vite-plugin-babel-macros";
 import path from "path";
 import dynamicImport from "vite-plugin-dynamic-import";
 import sass from "sass";
@@ -9,13 +9,7 @@ import sass from "sass";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    babel({
-      include: [/src/],
-      exclude: [/node_modules/],
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-      plugins: ["babel-plugin-macros"],
-      babelHelpers: "bundled",
-    }),
+    macrosPlugin(),
     react(),
     lingui(),
     dynamicImport(),
